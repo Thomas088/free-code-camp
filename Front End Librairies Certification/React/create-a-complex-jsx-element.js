73f4c9effba5b69fc4c2a6b63819f00f78a5b9e3
Thomas <thomaskouadio92@gmail.com>
2020-07-13 00:59:09 +0200

    add 2nd exercice

---
+++ Front End Librairies Certification/React/create-a-complex-jsx-element.js
@@ -0,0 +1,10 @@
+// Solution is here
+const JSX = (<div>
+                <h1>Title</h1>
+                <p>paragraph</p>
+                <ul>
+                  <li>list</li>
+                  <li>list</li>
+                  <li>list</li>
+                </ul>
+            </div> )
