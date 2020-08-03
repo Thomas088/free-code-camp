71b82ef0bceeaa45178a43b61ba2237033aa5acd
Thomas088 <thomaskouadio92@gmail.com>
2020-08-04 00:27:36 +0200

    create-a-react-component

---
+++ Front End Librairies Certification/React/create-a-react-component.js
@@ -0,0 +1,11 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // Solution is here
+    return (<div>
+                <h1>Hello React!</h1>
+            </div>);
+  }
+};
