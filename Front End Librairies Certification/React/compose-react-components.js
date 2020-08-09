695927c32994b6f832955c5d6a0a56d14834d819
Thomas088 <thomaskouadio92@gmail.com>
2020-08-09 02:50:59 +0200

    compose-react-components

---
+++ Front End Librairies Certification/React/compose-react-components.js
@@ -0,0 +1,31 @@
+class Fruits extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+        { /* Solution is here */ }
+        <NonCitrus/>
+        <Citrus/>
+      </div>
+    );
+  }
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+     super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        { /* Solution is here */ }
+        <Fruits/>
+        <Vegetables />
+      </div>
+    );
+  }
+};
